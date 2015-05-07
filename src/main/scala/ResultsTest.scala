8a97c7f58e67cb57d354116fb56652e302b3c13b
Thoralf Gutierrez <thoralf.gutierrez@realimpactanalytics.com>
2015-05-07 11:13:36 +0200

    change comments to scala doc

--- src/main/scala/ResultsTest.scala
+++ src/main/scala/ResultsTest.scala
@@ -1,8 +1,10 @@
 object ResultsTest {
+
+	/**
+	 * Execute the tests on the given files
+	 * @param args test-data-header test-data data-header data
+	 */
 	def main(args: Array[String]){
-		/*
-		 * args: test-data-header test-data data-header data
-		 */
 		val comp = OutputComparator(args(0), args(1), args(2), args(3))
 		CommandLineReport.report(comp)
 	}
